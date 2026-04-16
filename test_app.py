2307d216b7ac3925b74b5df2a49150eabd8627cc
saifkhandakar16 <saifkhandakar16@gmail.com>
2026-04-16 18:28:58 +0100

    Add Dash app test suite

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app
+
+def test_header_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_text_to_equal('h1', 'Pink Morsel Sales Visualiser', timeout=10)
+
+def test_visualisation_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#sales-chart', timeout=10)
+
+def test_region_picker_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element('#region-filter', timeout=10)
